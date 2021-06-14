e7c979f61b65b0faeeec800166da19138acbf3ac
Swapnil Phalke <71014235+swapnilphalke97@users.noreply.github.com>
2021-06-14 21:10:07 +0530

    Update Reversethearray.java

--- DSA450/Reversethearray.java
+++ DSA450/Reversethearray.java
@@ -1,1 +1,10 @@
-
+ static void rvereseArray(int arr[], int start, int end)
+    {
+        int t;
+        if (start >= end)
+            return;
+        t = arr[start];
+        arr[start] = arr[end];
+        arr[end] = t;
+        rvereseArray(arr, start+1, end-1);
+    }
