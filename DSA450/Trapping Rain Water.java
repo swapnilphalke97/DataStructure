de41b4bc7323c764bd4d9c15a5808090936cd617
Swapnil Phalke <71014235+swapnilphalke97@users.noreply.github.com>
2021-06-15 17:44:07 +0530

    Update Trapping Rain Water.java

--- DSA450/Trapping Rain Water.java
+++ DSA450/Trapping Rain Water.java
@@ -28,3 +28,30 @@ class Solution{
         
     }  
 }
+//proper solution
+class Solution{
+    
+    // arr: input array
+    // n: size of array
+    // Function to find the trapped water between the blocks.
+    static int trappingWater(int arr[], int n) { 
+       int[] l=new int[n];
+int[] r=new int[n];
+int temp=arr[0];
+int temp1=arr[n-1];
+for(int i=0;i<n;i++)
+{
+if(arr[i]>=temp){temp=arr[i];}l[i]=temp;}
+
+for(int i=n-1;i>=0;i--)
+{
+if(arr[i]>=temp1){
+temp1=arr[i];}r[i]=temp1;}
+int sum=0;
+for(int i=0;i<n;i++)
+{
+sum=sum+Math.min(l[i],r[i])-arr[i];
+}
+return sum;
+    }  
+}
